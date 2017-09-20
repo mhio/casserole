60066637a77d0e98146e51705b61e0ac38b88054
Matt Hoyle <code@deployable.co>
2017-09-20 23:05:58 +0000

    Base

---
+++ test/fixture/mocha-setup.js
@@ -0,0 +1,16 @@
+
+/* global chai */
+global.chai = require('chai')
+global.expect = chai.expect
+//chai.use(require('chai-fs'))
+//chai.use(require('chai-json-schema'))
+//chai.use(require('chai-as-promised'))
+
+require('bluebird').config({
+  longStackTraces: true,
+  warnings: true
+})
+
+if ( process.env.NODE_ENV === undefined ) {
+  process.env.NODE_ENV = 'test'
+}
---
+++ test/unit/0.js
@@ -0,0 +1,8 @@
+
+describe('unit::mh::forms::date', function(){
+
+  it(`${new Date()}`, function(){
+    expect(0).to.equal(0)
+  })
+  
+})
