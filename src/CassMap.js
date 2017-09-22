08200c8e46d7bdfd59036ec3443359125c650f7d
Matt Hoyle <code@deployable.co>
2017-09-22 08:28:37 +0000

    extend cassmap to supports names

--- src/CassMap.js
+++ src/CassMap.js
@@ -13,29 +13,60 @@ export default class CassMap {
 
   static classInit(){
     this.template = Util.template
-    this.valueToCqlMap = Util.valueToCqlMap
+    this.toCqlMap = Util.valueToCqlMap
   }
 
+  // Setup a debug instance for the class, noop if needed
   static debugInit(name){
     this.debug = debugr(name)
     if (!this.debug.enabled) this.debug = noop
   }
+  
+  constructor(data, name){
+    this._name = name
+    this._data = data
+  }
 
-  static toCqlMap(o){
-    return this.valueToCqlMap(o)
+  toObject(){
+    return this._data
   }
 
   toCqlMap(){
-    return this.constructor.valueToCqlMap(this._data)
+    return this.constructor.toCqlMap(this._data)
   }
 
-  addData(field, value){
+  toCql(){
+    if ( this.name === undefined ) throw new CassError(`Map must have a name to create cql`)
+    return `${this.name} = ${this.toCqlMap()}`
+  }
+
+  set name(name){
+    return this._name = name
+  }
+
+  get name(){
+    return this._name
+  }
+
+  get(field){
+    return this._data[field]
+  }
+
+  set(field, value){
+    return this._data[field] = value
+  }
+
+  add(field, value){
     if ( has(this._data, field) ) throw new CassError(`Map field ${field} already set`)
     return this._data[field] = value
   }
 
-  setData(field, value){
-    return this._data[field] = value
+  delete(field){
+    return delete this._data[field]
+  }
+
+  toJSON(){
+   return this._data
   }
 
 }
---
+++ test/unit/CassMap.js
@@ -0,0 +1,54 @@
+/* global expect */
+import CassMap from '../../src/CassMap'
+
+describe('unit::mh::casserole::CassMap', function(){
+
+  it('should have cassandra-drivers types attached', function(){
+    expect( CassMap.template ).to.be.a('function')
+    expect( CassMap.toCqlMap ).to.be.a('function')
+  })
+
+  let field = { name: 'a', type: 'int'}
+  let fields = { a: field }
+
+  let cassmap
+
+  beforeEach(function () {
+    cassmap = new CassMap({test: true, other: 'what'})
+  })
+
+  it('should have test set in the CassMap', function () {
+    expect( cassmap.get('test') ).to.equal(true)
+  })
+
+  it('should set "test" to false', function () {
+    cassmap.set('test', false)
+    expect( cassmap.get('test') ).to.equal(false)
+  })
+
+  it('should create a new CassMap without data', function () {
+    expect( new CassMap() ).to.be.ok
+  })
+
+  it('should return the data as object', function () {
+    expect( cassmap.toObject() ).to.eql({ test: true, other: 'what' })
+  })
+
+  it('should return the data as json', function () {
+    expect( cassmap.toJSON() ).to.eql({ test: true, other: 'what' })
+  })
+
+  it('should return the data as json', function () {
+    expect( JSON.stringify(cassmap) ).to.eql('{"test":true,"other":"what"}')
+  })
+
+  it('should return the data as cql map', function () {
+    expect( cassmap.toCqlMap() ).to.eql("{'test':'true','other':'what'}")
+  })
+
+  it('should return the data as cql map', function () {
+    cassmap.name = 'whatever'
+    expect( cassmap.toCql() ).to.eql("whatever = {'test':'true','other':'what'}")
+  })
+
+})
