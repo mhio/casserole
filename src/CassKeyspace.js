d5ca54759793972a11ecee81fb78317a455f943a
Matt Hoyle <code@deployable.co>
2017-09-22 14:07:28 +0000

    coverage

--- src/CassKeyspace.js
+++ src/CassKeyspace.js
@@ -2,6 +2,8 @@ import has from 'lodash/has'
 
 import Util from './Util'
 import CassCql from './CassCql'
+import CassError from './CassErrors'
+import CassReplicationStrategy from './CassReplicationStrategy'
 
 /*
 
@@ -28,7 +30,7 @@ import CassCql from './CassCql'
 
 */
 
-class CassKeyspace extends CassCql {
+export default class CassKeyspace extends CassCql {
 
   static classInit(){
     this.debugInit('mh:casserole:CassKeyspace')
@@ -50,13 +52,15 @@ class CassKeyspace extends CassCql {
     let exists_clause = (options.q_if_not_exists)
       ? this.create_exists_cql
       : ''
+
     let durable = (has(options,'q_durable'))
       ? Util.template(this.durable_cql, `${Boolean(options.q_durable)}`)
       : Util.template(this.durable_cql, 'true')
-    this.debug('durable', options.q_durable, durable)
     let options_cql = durable
+
+    CassError.if( !replication_stategy, 'No replication strategy' )
     let replication_stategy_cql = (replication_stategy.toCqlMap)
-      ? replication_stategy_cql.toCqlMap()
+      ? replication_stategy.toCqlMap()
       : Util.valueToCqlMap(replication_stategy)
 
     return Util.template(
@@ -77,20 +81,19 @@ class CassKeyspace extends CassCql {
   constructor( name, options = {} ){
     super()
     this.keyspace = name
-    this.fields = {}
-    this.replication = options.replication
+    this.replication = options.replication || CassReplicationStrategy.simple()
     this.durable = (has(options.durable)) ? Boolean(options.durable) : true
   }
 
-  addField(field, type){
-    this.fields[field] = { name: field, type: type }
+  get keyspace(){ return this._keyspace }
+  set keyspace( value ){
+    CassError.if( !value, 'No keyspace name')
+    this._keyspace = value
   }
 
   toCqlCreate(){
-    return this.constructor.toCreateCql(this.keyspace, this.replication, this.durable)
+    return this.constructor.toCqlCreate(this.keyspace, this.replication, this.durable)
   }
 
 }
 CassKeyspace.classInit()
-
-export default CassKeyspace
--- test/unit/CassKeyspace.js
+++ test/unit/CassKeyspace.js
@@ -1,5 +1,6 @@
 /* global expect */
 import CassKeyspace from '../../src/CassKeyspace'
+import CassError from '../../src/CassErrors'
 
 describe('unit::mh::casserole::CassKeyspace', function(){
 
@@ -23,6 +24,18 @@ describe('unit::mh::casserole::CassKeyspace', function(){
     expect( CassKeyspace.toCqlCreate('one', {rf:2}, {q_if_not_exists: true}) ).to.equal(
       'CREATE KEYSPACE IF NOT EXISTS one WITH REPLICATION = {\'rf\':2} AND DURABLE_WRITES = true;'
     )
+  })
+
+  it('should create a keyspace instance', function () {
+    expect( new CassKeyspace('whatever') ).to.be.ok    
+  })
+
+  it('should fail to create a keyspace instance without name', function () {
+    expect( ()=> new CassKeyspace() ).to.throw(CassError)
+  })
+
+  it('should create a keyspace instance', function () {
+    expect( new CassKeyspace('whatever').toCqlCreate() ).to.equal('CREATE KEYSPACE  whatever WITH REPLICATION = {\'class\':\'SimpleStrategy\',\'replication_factor\':3} AND DURABLE_WRITES = true;')
   })
 
 })
