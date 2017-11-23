8e170be6a0e467e9cfd01c9728154a98e619a81e
Matt Hoyle <code@deployable.co>
2017-11-23 01:49:12 +0000

    Add src build

--- index.js
+++ index.js
@@ -1,2 +1,38 @@
-console.log('casserole')
 
+var CassCql     = require('./lib/CassCql')
+var CassEntity  = require('./lib/CassEntity')
+var CassErrors  = require('./lib/CassErrors')
+var CassInsert  = require('./lib/CassInsert')
+var CassKeyspace  = require('./lib/CassKeyspace')
+var CassMap     = require('./lib/CassMap')
+var CassQuery   = require('./lib/CassQuery')
+var CassReplicationStrategy  = require('./lib/CassReplicationStrategy')
+var CassSelect  = require('./lib/CassSelect')
+var CassTable   = require('./lib/CassTable')
+var CassType    = require('./lib/CassType')
+var CassValue   = require('./lib/CassValue')
+var Client      = require('./lib/Client')
+var Model       = require('./lib/Model')
+var Paramaters  = require('./lib/Paramaters')
+var Schema      = require('./lib/Schema')
+var Util        = require('./lib/Util')
+
+module.exports = {
+  CassCql: CassCql,
+  CassEntity: CassEntity,
+  CassErrors: CassErrors,
+  CassInsert: CassInsert,
+  CassKeyspace: CassKeyspace,
+  CassMap: CassMap,
+  CassQuery: CassQuery,
+  CassReplicationStrategy: CassReplicationStrategy,
+  CassSelect: CassSelect,
+  CassTable: CassTable,
+  CassType: CassType,
+  CassValue: CassValue,
+  Client: Client,
+  Model: Model,
+  Paramaters: Paramaters,
+  Schema: Schema,
+  Util: Util,
+}
