e204c8c7a407922a19f93c0aac41a91b28260db5
Matt Hoyle <code@deployable.co>
2018-03-28 02:52:06 +0000

    check built test did something

--- test/built/simple.js
+++ test/built/simple.js
@@ -3,30 +3,44 @@ const { Client, Model } = require('../../')
 
 describe('mhio::test::built::casserole', function(){
 
-  let client = null
-
-  after(function(){
-    return (client) ? client.disconnect() : null
-  })
-
   it('should require the files', function(){
     expect(Client).to.be.a('function')
     expect(Model).to.be.a('function')
   })
 
-  it('should do some stuff', function(){
-    client = new Client('my_keyspace')
-    let MyModel = Model.generate(
-      'MyModel',
-      { id: { type:'uuid', primary: true }, name: 'string', count: 'integer' },
-      { client: client }
-    )
-    return client.connect()
-      .then(() => MyModel.sync())
-      .then(() => {
-        let mym = new MyModel({ id: '12341234-1234-1234-1234-123412341234', name: 'kimmy', count: 5 })
-        return mym.execSave()
-      })
+  describe('connections', function(){
+    
+    let client = null
+
+    before('client connect', async function(){
+      client = new Client('casserole_test_built')
+      await client.connect()
+    })
+
+    after('client disconnect', function(){
+      return client.disconnect()
+    })
+
+
+    it('should do some stuff', function(){
+      let MyModel = Model.generate(
+        'MyModel',
+        { id: { type:'uuid', primary: true }, name: 'string', count: 'integer' },
+        { client: client }
+      )
+      return client.connect()
+        .then(() => {
+          let mym = new MyModel({ id: '12341234-1234-1234-1234-123412341234', name: 'kimmy', count: 5 })
+          return mym.execSave()
+        })
+    })
+
+    it('should select the stuff', async function(){
+      let res = await client.select('my_models', '*', {id: '12341234-1234-1234-1234-123412341234' })
+      expect( res.rowLength ).to.equal(1)
+      expect( res.rows[0].name ).to.equal('kimmy')
+    })
+
   })
 
 })
