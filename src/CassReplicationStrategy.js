22c7e79c976bb7b48aa9d1856ceea0b9d1d449b9
Matt Hoyle <code@deployable.co>
2018-04-03 01:35:12 +0000

    formatting

--- src/CassReplicationStrategy.js
+++ src/CassReplicationStrategy.js
@@ -22,8 +22,8 @@ class CassReplicationStrategy extends CassMap {
   }
 
   // Create a Simple strategy
-  static simple(replication_factor = 3){
-    if (!isInteger(replication_factor)) {
+  static simple( replication_factor = 3 ){
+    if ( !isInteger(replication_factor) ){
       throw new CassException('SimpleStrategy needs an integer replication_factor')
     }
     return new this('SimpleStrategy', { replication_factor: replication_factor })
@@ -31,7 +31,7 @@ class CassReplicationStrategy extends CassMap {
 
   // Create a NetworkTopologyStrategy strategy
   static networkTopology( dcs ){
-    if ( isEmpty(dcs) || !isPlainObject(dcs) ) {
+    if( isEmpty(dcs) || !isPlainObject(dcs) ){
       throw new CassException(
         'NetworkTopologyStrategy needs an object containing data center replication factors',
         { label: 'ReplicationStrategy', details: dcs }
@@ -41,10 +41,11 @@ class CassReplicationStrategy extends CassMap {
     return new this('NetworkTopologyStrategy', dcs)
   }
 
-  /** new CassReplicationStrategy
-   *  @param {String} replication_class - The replication class name to create
-   *  @param {Object} data              - The configuration data for the class
-   */
+  /** 
+  * new CassReplicationStrategy
+  * @param {String} replication_class - The replication class name to create
+  * @param {Object} data              - The configuration data for the class
+  */
   constructor( replication_class, data = {} ){
     super()
     this._data = {}
@@ -58,17 +59,17 @@ class CassReplicationStrategy extends CassMap {
   }
 
   // This is annoying as the user might expect `strat.dc = 1` to work the same way :/
-  set class (value) { 
-    if ( !this.constructor.replication_strategies.includes(value) ) {
+  set class( value ){
+    if ( !this.constructor.replication_strategies.includes(value) ){
       throw new CassException(
         `Replication Strategy class must be one of [${this.constructor.replication_strategies}]". Got ${value}`,
         { detail: {value: value} }
       )
-    } 
-      
+    }
     return this._data.class = value
   }
-  get class (){
+
+  get class(){
     return this._data.class
   }
 
